56a879055973a0e4c1da472c0a5633b0b4ace9b7
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-03-25 21:13:27 -0300

    [Feature] MUI theme settings

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,35 @@ import { Sales } from './routes/Sales';
 import { Kitchen } from './routes/Kitchen';
 import { Delivery } from './routes/Delivery';
 import { Admin } from './routes/Admin';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import { Header } from './components/global/Header';
+import { Footer } from './components/global/Footer';
+import { ColorModeContext, useMode } from './Theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  const { theme, colorMode } = useMode();
 
   return (
-    <>
-    <Header />
-      <Routes>
-        <Route path='/' element={<Login />}/>
-        <Route path= '/user' element={<User />} />
-        <Route path= '/sales' element={<Sales />} />
-        <Route path= '/kitchen' element={<Kitchen />} />
-        <Route path= '/delivery' element={<Delivery />} />
-        <Route path= '/admin' element={<Admin />} />
-      </Routes>
-    <Footer />
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+        <Header />
+        <main className='content'>
+          <Routes>
+            <Route path='/' element={<Login />}/>
+            <Route path= '/user' element={<User />} />
+            <Route path= '/sales' element={<Sales />} />
+            <Route path= '/kitchen' element={<Kitchen />} />
+            <Route path= '/delivery' element={<Delivery />} />
+            <Route path= '/admin' element={<Admin />} />
+          </Routes>
+        </main>
+        <Footer />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App
